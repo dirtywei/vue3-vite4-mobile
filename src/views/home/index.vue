7ef67fe5179cf3cd55bf8e0f984bb9c176f91217
dirtywei <gdpu_ysw@163.com>
2023-03-02 22:55:59 +0800

    feat:  增加sass

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div> Home </div>
+  <div class="home"> Home </div>
 </template>
 
 <script setup>
@@ -9,4 +9,10 @@
   sStorage.set('TEST_LS', '你好啊')
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+  .home {
+    width: 200px;
+    height: 300px;
+    background-color: #000;
+  }
+</style>
